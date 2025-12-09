4ab63387c2a275dd910f6eb5e1fd15d9015d24c8
Zizou10 <paolo.pilati93@gmail.com>
2025-12-09 12:22:31 +0100

    Errore nell'ordine, le condizioni sembrano giuste di per sé ma "FizzBuzz" non arriva a essere stampato, forse perché va stabilita prima delle altre condizioni?

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 //1 Stampare numeri da 1 a 100
-//2 Selezionare multipli di 3
-//3 Selezionare multipli di 5
+//2 Selezionare multipli di 3 (% 3)
+//3 Selezionare multipli di 5 (% 5)
+//4 Selezionare multipli di 3 e 5
+//5 Restituire ciclo finale corretto
 
 
 //1
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+
+  else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+
+  else {
+    console.log(i);
+  }
+
 }
